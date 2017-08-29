3298674286ffe9b2332133ed2f65b2bf99466a58
taitanxiami <taitanxiami@gmail.com>
2017-08-29 18:04:30 +0800

    add iconv-lite

---
+++ file/showlrc.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+const fs = require('fs');
+const iconv = require('iconv-lite');
+
+fs.readFile(path.join(__dirname,'./李炜 - 你不爱我.lrc'), function(error, data){
+  
+  if(error) throw error;
+  const lines = iconv.decode(data,'gbk').split('\n');
+  // [00:07.30]www.22lrc.com ��������
+  var regex = /\[(\d{2})\:(d{2})\.(d{2})\](.+)/;
+  lines.forEach(function(line){
+      
+    
+    const matched = regex.exec(line);
+
+    if(matched) {
+
+      const h = parseFloat(matched[1]);
+      const m = parseFloat(matched[2]);
+      const s = parseFloat(matched[3]);
+      //到达指定时间执行一次
+      setTimeout(function(){
+        console.log(line);     
+      }, s  + m  * 1000 + h * 60  * 1000);
+    }else {
+      console.log(line);
+    }
+  
+
+  });
+});
