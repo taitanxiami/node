58153ed71ee5bea416fbd6498bfe82f858502eab
taitanxiami <taitanxiami@gmail.com>
2017-08-29 23:01:02 +0800

    add offsetTime to correct lrc

--- file/showlrc.js
+++ file/showlrc.js
@@ -8,18 +8,19 @@ fs.readFile(path.join(__dirname, './李炜 - 你不爱我.lrc'), function(error,
     const lines = iconv.decode(data, 'gbk').split('\n');
     // [00:16.10]习惯被拒绝的人会先拒绝
     var regex = /\[(\d{2})\:(\d{2})\.(\d{2})\](.+)/;
+    const beginTime = new Date();
     lines.forEach(function(line) {
         const matched = regex.exec(line);
-
         if (matched) {
 
+            const offsetTime = new Date() - beginTime.getTime();
             const h = parseFloat(matched[1]);
             const m = parseFloat(matched[2]);
             const s = parseFloat(matched[3]);
             //到达指定时间执行一次
             setTimeout(function() {
                 console.log(line);
-            }, s + m * 1000 + h * 60 * 1000);
+            }, s + m * 1000 + h * 60 * 1000 - offsetTime);
         } else {
             console.log(line);
         }
