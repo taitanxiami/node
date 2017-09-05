bf4924b5142e6818117341062ebde242b068f960
taitanxiami <taitanxiami@gmail.com>
2017-09-05 08:12:34 +0800

    add class

---
+++ module/Person.js
@@ -0,0 +1,18 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    // this.run() = function() {
+    //     console.log('person can run');
+    // };
+}
+Person.prototype.sayHello = function() {
+    console.log('Person say hello');
+}
+
+Person.prototype = {
+        saysong: function() {
+            console.log('唱歌');
+        }
+    }
+    //暴露出来一个类,此时Person 就被视为一个构造函数
+module.exports = Person;
